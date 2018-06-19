4a5e1e88eb51480a650f16039f870b7a9260c9bf
Eftimie Andrei <bax@baxandrei.ro>
2018-06-19 17:27:42 +0300

    Create language.php

---
+++ upload/modules/VotingPlugin/language/Romanian/language.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr4
+ *
+ *  License: MIT
+ *
+ *  Romanian Language for VotingPlugin module
+ *  Translation By @BaxAndrei ( https://baxandrei.ro )
+ *  Last Update: 19/06/2018
+ */
+$language = array(
+	'vote' => 'Votează',
+	'top_voters' => 'Votanți de top',
+	'username' => 'Nume de utilizator',
+	'monthly_votes' => 'Voturi lunare',
+	'weekly_votes' => 'Voturi săptămânale',
+	'daily_votes' => 'Voturi zilnice',
+	'all_time_votes' => 'Toate voturile',
+	'vote_sites' => 'Site-urile de vot',
+	'unable_to_get_data' => 'Imposibil de obţinut datele',
+	'new_vote_site' => 'Site de vot nou',
+	'no_vote_sites' => 'Niciun site de vot nu a fost definit încă.',
+	'site_name' => 'Numele site-ului',
+	'site_url' => 'Adresa URL completă a site-ului (cu http://)',
+	'confirm_delete_vote_site' => 'Sigur doriți să ștergeți acest site de vot?',
+	'name_required' => 'Introduceți un nume de site',
+	'url_required' => 'Introduceți un URL pentru site',
+	'url_min_8' => 'Asigurați-vă că adresa URL a site-ului dvs. are cel puțin 8 caractere',
+	'name_max_64' => 'Asigurați-vă că numele site-ului este de maximum 64 de caractere',
+	'url_max_256' => 'Asigurați-vă că adresa URL a site-ului este de maximum 256 de caractere',
+	'site_edited_successfully' => 'Site-ul a fost editat cu succes.',
+	'this_month' => 'Luna aceasta',
+	'this_week' => 'Săptămâna aceasta',
+	'today' => 'Astăzi',
+	'all_time' => 'Tot timpul',
+	'order' => 'Ordonează'
+);

6a8bd6e70c85134192c5edd25c97914360262e08
Fjuro <40403606+Fjuro@users.noreply.github.com>
2021-01-09 14:35:05 +0100

    Create language.php

---
+++ upload/modules/VotingPlugin/language/Czech/language.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ *	Made by Samerton
+ *  Translated by Fjuro
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr2
+ *
+ *  License: MIT
+ *
+ *  Czech Language for VotingPlugin module
+ */
+
+$language = array(
+	'vote' => 'Hlasování',
+	'top_voters' => 'Nejlepší hlasující',
+	'username' => 'Uživatelské jméno',
+	'monthly_votes' => 'Měsíční hlasy',
+	'weekly_votes' => 'Týdenní hlasy',
+	'daily_votes' => 'Denní hlasy',
+	'all_time_votes' => 'Hlasy za celou dobu',
+	'vote_sites' => 'Hlasovací weby',
+	'unable_to_get_data' => 'Nepodařilo se získat data',
+	'new_vote_site' => 'Nový hlasovací web',
+	'no_vote_sites' => 'Nebyly nastaveny žádné hlasovací weby.',
+	'site_name' => 'Název webu',
+	'site_url' => 'Celá URL webu <em>(s předchozím http://)</em>',
+	'confirm_delete_vote_site' => 'Opravdu chcete odstranit tento hlasovací web?',
+	'name_required' => 'Zadejte název webu',
+	'url_required' => 'Zadejte URL webu',
+	'url_min_8' => 'Ujistěte se, že URL vašeho webu obsahuje minimálně 8 znaků včetně http://',
+	'name_max_64' => 'Ujistěte se, že název vašeho webu obsahuje maximálně 64 znaků',
+	'url_max_256' => 'Ujistěte se, že URL vašeho webu obsahuje maximálně 256 znaků',
+	'site_edited_successfully' => 'Web úspěšně upraven.',
+	'this_month' => 'Tento měsíc',
+	'this_week' => 'Tento týden',
+	'today' => 'Dnes',
+	'all_time' => 'Za celou dobu',
+	'order' => 'Pořadí',
+	'icon_example' => 'Příklad: <i class="fa fa-thumbs-up" aria-hidden="true"></i>',
+	'message' => 'Zpráva k zobrazení na vrchu hlasovací stránky <em>(může být ponecháno prázdné)</em>',
+	'message_maximum' => 'Vaše hlasovací zpráva může obsahovat maximálně 2048 znaků',
+	'edit_site' => 'Úprava webu',
+	'delete_site' => 'Opravdu chcete odstranit tento web?',
+	'site_created_successfully' => 'Web úspěšně vytvořen.',
+	'site_deleted_successfully' => 'Web úspěšně odstraněn.',
+	'updated_successfully' => 'Úspěšně aktualizováno',
+	'config_not_writable' => 'Nepodařilo se vytvořit konfigurační soubor doplňku VotingPlugin. Ujistěte se, že je adresář <strong>modules/VotingPlugin</strong> upravitelný a zakažte + znovu povolte doplněk.',
+	'please_configure_module' => 'Nejprve prosím nastavte doplněk v souboru <strong>modules/VotingPlugin/config.php</strong>.'
+);

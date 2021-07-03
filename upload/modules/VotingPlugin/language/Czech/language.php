8af23e34745e92ba4829788e21d3dfc79b2fd0f1
Fjuro <fjuro@seznam.cz>
2021-07-03 11:44:31 +0200

    Create language.php

---
+++ upload/modules/VotingPlugin/language/Czech/language.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ *	Made by Samerton
+ *  Translated by Fjuro
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr2
+ *
+ *  License: MIT
+ *
+ *  Czech Language for VotingPlugin module
+ */
+
+$language = array(
+	'vote' => 'Hlasování',
+	'top_voters' => 'Nejlepší hlasující',
+	'username' => 'Jméno',
+	'monthly_votes' => 'Měsíční hlasy',
+	'weekly_votes' => 'Týdenní hlasy',
+	'daily_votes' => 'Denní hlasy',
+	'all_time_votes' => 'Hlasy za celou dobu',
+	'vote_sites' => 'Hlasovací stránky',
+	'unable_to_get_data' => 'Nepodařilo se získat data',
+	'new_vote_site' => 'Nová hlasovací stránka',
+	'no_vote_sites' => 'Zatím nebyly nastaveny žádné hlasovací stránky.',
+	'site_name' => 'Název stránky',
+	'site_url' => 'Celá URL stránky <em>(s předchozím http://)</em>',
+	'confirm_delete_vote_site' => 'Opravdu chcete odebrat tuto hlasovací stránku?',
+	'name_required' => 'Zadejte prosím název stránky',
+	'url_required' => 'Zadejte prosím URL stránky',
+	'url_min_8' => 'Ujistěte se, že URL vaší stránky obsahuje alespoň 8 znaků, včetně http://',
+	'name_max_64' => 'Ujistěte se, že název vaší stránky má maximálně 64 znaků',
+	'url_max_256' => 'Ujistěte se, že URL vaší stránky má maximálně 256 znaků',
+	'site_edited_successfully' => 'Stránka úspěšně upravena.',
+	'this_month' => 'Tento měsíc',
+	'this_week' => 'Tento týden',
+	'today' => 'Dnes',
+	'all_time' => 'Za celou dobu',
+	'order' => 'Pořadí',
+	'icon_example' => 'Například: <i class="fa fa-thumbs-up" aria-hidden="true"></i>',
+	'message' => 'Zpráva k zobrazení navrchu hlasovací stránky <em>(může být ponecháno prázdné)</em>',
+	'message_maximum' => 'Vaše hlasovací zpráva může obsahovat maximálně 2048 znaků',
+	'edit_site' => 'Úprava stránky',
+	'delete_site' => 'Opravdu chcete odebrat tuto stránku?',
+	'site_created_successfully' => 'Stránka úspěšně vytvořena.',
+	'site_deleted_successfully' => 'Stránka úspěšně odstraněna.',
+	'updated_successfully' => 'Úspěšně upraveno',
+	'config_not_writable' => 'Nepodařilo se vytvořit konfigurační soubor doplňku. Ujistěte se, že je adresář <strong>modules/VotingPlugin</strong> zapisovatelný a poté zakažte a znovu povolte doplněk.',
+	'please_configure_module' => 'Nejprve prosím nastavte doplněk v souboru <strong>modules/VotingPlugin/config.php</strong>.',
+	'link_location' => 'Umístění odkazu',
+	'icon' => 'Ikona',
+
+	// Validation
+	'site_name_required' => 'Je vyžadován název stránky',
+	'site_name_minimum' => 'Název stránky musí obsahovat alespoň 2 znaky',
+	'site_name_maximum' => 'Název stránky může obsahovat maximálně 64 znaků',
+	'site_url_required'=> 'Je vyžadována URL stránky',
+	'site_url_minimum' => 'URL stránky musí obsahovat alespoň 10 znaků',
+	'site_url_maximum' => 'URL stránky může obsahovat maximálně 255 znaků'
+
+);

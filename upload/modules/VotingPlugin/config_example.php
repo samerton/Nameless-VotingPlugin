19021b9d0b283cecd8e57f0984045753fef19831
poqdavid <poq-david@live.com>
2022-10-03 05:11:40 -0400

    Added example config

---
+++ upload/modules/VotingPlugin/config_example.php
@@ -0,0 +1,9 @@
+<?php
+$voting_plugin = array(
+'host' => '127.0.0.1',
+'port' => 3306,
+'user' => 'admin',
+'database' => 'votingplugin',
+'password' => 'admin',
+'table' => 'VotingPlugin_Users'
+);
